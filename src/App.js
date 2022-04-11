e5da637d627e4df9df0b3f2ab1eed6a5f058a9b5
Régis GRARE <regisgrare@hotmail.fr>
2022-04-11 10:54:23 +0200

    store OK

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import PostForm from "./components/PostForm";
 import User from "./components/User";
 
 const App = () => {
+
+  const posts = useSelector((state) => state.postReducer)
+
+  console.log(posts);
   return (
     <div>
       <h1>Extreme</h1>
--- src/index.js
+++ src/index.js
@@ -3,4 +3,29 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/index.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+// REDUX
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension"
+import thunk from "redux-thunk";
+import rootReducer from "./reducers"
+import { getPosts } from "./actions/post.action";
+
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk)))
+
+store.dispatch(getPosts())
+
+
+
+ReactDOM.render(
+    <Provider store={store}>
+
+        <App />
+
+    </Provider>,
+
+    document.getElementById("root"));
