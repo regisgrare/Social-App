e866b3371f9bce9e2d0b3c9dd38c26bf4973903d
Régis GRARE <regisgrare@hotmail.fr>
2022-04-11 14:33:07 +0200

    likes OK

--- src/components/Like.js
+++ src/components/Like.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { addLike } from "../actions/post.action";
 
 const Like = ({ post }) => {
+
+  const user = useSelector((state) => state.userReducer)
+
+  const dispatch = useDispatch()
+  const handleLike = () => {
+    const postData = {
+      title: post.title,
+      author: post.author,
+      content: post.content,
+      likes: ++post.likes,
+      id: post.id
+    }
+    const userData = {
+      pseudo: user[0].pseudo,
+      likes: ++user[0].likes,
+      id: user[0].id
+    }
+    dispatch(addLike(postData))
+  }
   return (
-    <div>
-      <img src="./icons/clap.png" className="clap" alt="clap" />
+    <div onClick={handleLike}>
+      <img
+        src="./icons/clap.png"
+        className="clap"
+        alt="clap" />
       <span>{post.likes}</span>
     </div>
   );
--- src/reducers/post.reducer.js
+++ src/reducers/post.reducer.js
@@ -1,4 +1,4 @@
-import { ADD_POST, DELETE_POST, EDIT_POST, GET_POSTS } from "../actions/post.action";
+import { ADD_LIKE, ADD_POST, DELETE_POST, EDIT_POST, GET_POSTS } from "../actions/post.action";
 
 
 const initialState = {}
@@ -20,6 +20,16 @@ export default function postReducer(state = initialState, action) {
             })
         case DELETE_POST:
             return state.filter((post) => post.id !== action.payload.postId)
+
+        case ADD_LIKE:
+            return state.map((post) => {
+                if (post.id === action.payload.id) {
+                    return {
+                        ...post,
+                        likes: action.payload.likes
+                    }
+                } else return post
+            })
         default:
             return state;
     }
